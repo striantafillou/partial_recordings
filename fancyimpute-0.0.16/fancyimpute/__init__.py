49b2f9b4a95196802aa560a7ae15c27399065175
striantafillou <sof.triantafillou@gmail.com>
2017-05-10 11:02:14 -0500

    added NN with MVI

---
+++ fancyimpute-0.0.16/fancyimpute/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function, division
+
+from .solver import Solver
+from .nuclear_norm_minimization import NuclearNormMinimization
+from .bayesian_ridge_regression import BayesianRidgeRegression
+from .mice import MICE
+from .auto_encoder import AutoEncoder
+from .matrix_factorization import MatrixFactorization
+from .iterative_svd import IterativeSVD
+from .simple_fill import SimpleFill
+from .soft_impute import SoftImpute
+from .biscaler import BiScaler
+from .knn import KNN
+from .similarity_weighted_averaging import SimilarityWeightedAveraging
